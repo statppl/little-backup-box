099a6a6e16b01d1fbeba420863734c6b9967db8c
outdoorbits <stefan@saams.de>
2026-08-23 18:29:01 +0200

    disable captive portal support (still alpha)

--- scripts/sub-menu.php
+++ scripts/sub-menu.php
@@ -80,14 +80,13 @@
 						<?php
 					}
 
-					<?php /*
 					// captive portal support is still alpha
-					if (is_file($constants['const_NETWORK_PORTAL_PAGE_LINK_FILE']) && filesize($constants['const_NETWORK_PORTAL_PAGE_LINK_FILE']) > 0) {# xxx and protocol is http
-						?>
-							<li class="nav-item"><a class="nav-link<?php echo $scriptname=="captive_portal"?" active":""; ?>" href="/frame.php?page=captive_portal"><?php echo L::mainmenue_captive_portal; ?></a></li>
-						<?php
-					}
-					*/ ?>
+					// if (is_file($constants['const_NETWORK_PORTAL_PAGE_LINK_FILE']) && filesize($constants['const_NETWORK_PORTAL_PAGE_LINK_FILE']) > 0) {# xxx and protocol is http
+					// 	?>
+					// 		<li class="nav-item"><a class="nav-link<?php echo $scriptname=="captive_portal"?" active":""; ?>" href="/frame.php?page=captive_portal"><?php echo L::mainmenue_captive_portal; ?></a></li>
+					// 	<?php
+					// }
+
 				?>
 			</ul>
 		</div>
